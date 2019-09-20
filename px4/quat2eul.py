f200d31d733163c7f7d8a62a88d6825bbc526e5d
Simeon Karnes <39631648+Simeon-Karnes@users.noreply.github.com>
2019-09-20 17:32:28 -0500

    Updated RPY quinternion to degrees conversion

--- px4/quat2eul.py
+++ px4/quat2eul.py
@@ -8,15 +8,30 @@ Purpose:        Quick and dirty quat2eul converter to be used in px4 parsing
 
 import math
 
+#def quat2eul(Qx, Qy, Qz, Qw):
+#    
+#    roll = []
+#    pitch = []
+#    yaw = []
+#    
+#    for qx, qy, qz, qw in zip(Qx, Qy, Qz, Qw):
+#        yaw.append(math.degrees(math.atan2(2*qy*qw-2*qz*qz, 1-2*qy**2-2*qz**2)))
+#        pitch.append(math.degrees(math.asin(2*qx*qy+2*qz*qw)))
+#        roll.append(math.degrees(math.atan2(2*qx*qw-2*qy*qz, 1-2*qx**2-2*qz**2)))
+#    
+#    return yaw,pitch,roll
+
+
 def quat2eul(Qx, Qy, Qz, Qw):
     
     roll = []
     pitch = []
     yaw = []
     
-    for qx, qy, qz, qw in zip(Qx, Qy, Qz, Qw):
-        yaw.append(math.degrees(math.atan2(2*qy*qw-2*qz*qz, 1-2*qy**2-2*qz**2)))
-        pitch.append(math.degrees(math.asin(2*qx*qy+2*qz*qw)))
-        roll.append(math.degrees(math.atan2(2*qx*qw-2*qy*qz, 1-2*qx**2-2*qz**2)))
+    for q_0, q_1, q_2, q_3 in zip(Qx, Qy, Qz, Qw):
+        
+        roll.append(math.degrees(math.atan2((q_2*q_3) + (q_0*q_1) , 0.5-((q_1**2)+(q_2**2)))))
+        pitch.append(math.degrees(math.asin(-2*((q_1*q_3)-(q_0*q_2)))))
+        yaw.append(math.degrees(math.atan2((q_1*q_2)+(q_0*q_3) , 0.5-(q_2**2+q_3**2))))
     
-    return yaw,pitch,roll
+    return roll,pitch,yaw
